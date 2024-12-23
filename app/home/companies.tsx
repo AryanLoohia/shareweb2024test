53812e1007226fbecd2c9c7625ef903813cb1dc8
Aryan Lohia <aryanlohia2005@gmail.com>
2024-12-23 15:16:13 +0530

    update

--- app/home/companies.tsx
+++ app/home/companies.tsx
@@ -12,7 +12,8 @@ const Companies = () => {
                 ul.insertAdjacentHTML('afterend', ul.outerHTML);
                 ul.nextSibling.setAttribute('aria-hidden', 'true');
             })"
-          className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+          className="w-full inline-flex flex-nowrap overflow
+          -hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
         >
           <ul
             x-ref="logos"
