2d2f2ffdb44207eea4544eb7cea1a2021fdc8456
Aryan Lohia <aryanlohia2005@gmail.com>
2025-03-01 12:20:59 +0530

    fixed spellings

--- app/home/events.tsx
+++ app/home/events.tsx
@@ -88,7 +88,7 @@ const SkeletonFour = () => {
   return (
     <div>
       <p className="font-bold md:text-4xl text-xl text-white">
-        Equity Reasearch
+        Equity Research
       </p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
